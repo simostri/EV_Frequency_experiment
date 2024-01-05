14bc49ef3ffc8e04edc6f993fe305bb99466428a
Simone <sistri@dtu.dk>
2024-01-05 14:43:36 +0100

    Intermediate step toward the change of the DEMO_postprocessing_SS_1 to a function based code, together with adding the normalization of the frequency for the calculation of the cross-correlation

--- DEMOPostProcessing_SS_v1.py
+++ DEMOPostProcessing_SS_v1.py
@@ -21,128 +21,158 @@ plt.rcParams['ytick.labelsize'] = 20
 
 DefFontSize = 20
 EVSEcolors = ['blue', 'red', 'green']
-data_PIOT = pd.read_csv('./Data/prova1DEMO2023.csv')
 
-t_start_long = data_PIOT['timestamp'].iloc[1500]
-t_end_long = data_PIOT['timestamp'].iloc[-450]
-data_PIOT_long = data_PIOT[(data_PIOT['timestamp'] >= t_start_long) & (data_PIOT['timestamp'] <= t_end_long)]
-time_PIOT_long = data_PIOT_long['timestamp']
-time_PIOT_long= pd.to_datetime(time_PIOT_long, unit='ms')
-delay = time_PIOT_long.diff()
-
-
-t_start_short = data_PIOT['timestamp'].iloc[-650]
-t_end_short = data_PIOT['timestamp'].iloc[-450]
-data_PIOT_short = data_PIOT[(data_PIOT['timestamp'] >= t_start_short) & (data_PIOT['timestamp'] <= t_end_short)]
-time_PIOT_short = data_PIOT_short['timestamp']
-time_PIOT_short= pd.to_datetime(time_PIOT_short, unit='ms')
-
-
-ACP = -data_PIOT_long['REG_LINE_ACP_1'] - data_PIOT_long['REG_LINE_ACP_2'] - data_PIOT_long['REG_LINE_ACP_3']
-freq = data_PIOT_long['REG_FREQUENCY']
-
-cross_correlation = np.correlate(ACP - np.mean(ACP),freq- np.mean(freq),  mode='full')
-lags = np.arange(-(len(freq) - 1), len(freq))
-
-plt.figure(figsize=(10, 6))
-plt.plot(lags, cross_correlation-60)
-plt.xlim(0, 16) 
-
-plt.title('Cross-correlation between Curve 1 and Curve 2')
-plt.xlabel('Lags')
-plt.ylabel('Cross-correlation')
-plt.grid(True)
-plt.savefig('./graphs/Cross_corr.pdf')
-
-lag_at_max_corr = lags[np.argmax(cross_correlation)]
-print("The lag is:", lag_at_max_corr, "time units")
-
-rmse = np.sqrt(np.mean((np.array(ACP) - np.array(freq))**2))
-average_error = rmse / len(ACP)
+file_path = './Data/prova1DEMO2023.csv'
+start_long_index = 1500
+end_long_index = -450
+start_short_index = -650
+end_short_index = -450
+include_plot = True
+include_plot_P_vs_freq = False
+include_plot_individual_values = False
 
 
 
-fig, (ax1, ax5) = plt.subplots(nrows=2, ncols=1, figsize=(15, 7), sharex=False)
-# First subplot
-ax1.grid(True)
-line1, = ax1.plot(time_PIOT_long, data_PIOT_long['REG_FREQUENCY'], label='Measured frequency', color=EVSEcolors[0])
-ax1.set_ylim(49.9, 50.1) 
-ax1.set_ylabel('Frequency [Hz]', fontsize=DefFontSize * 1.2, color=EVSEcolors[0])
-ax1.set_xlabel('Time [hh:mm]', fontsize=DefFontSize * 1)
-xticks = ax1.get_xticks()
-xticklabels = ax1.get_xticklabels()
-ax1.set_xticks(xticks)
-ax1.set_xticklabels(xticklabels, rotation=90)
-ax2 = ax1.twinx()
-line2, = ax2.plot(time_PIOT_long, -data_PIOT_long['REG_LINE_ACP_1'] - data_PIOT_long['REG_LINE_ACP_2'] - data_PIOT_long['REG_LINE_ACP_3'], color=EVSEcolors[1], label='Cluster Power')
-ax2.set_ylim(12.25, 17.5)
-ax2.set_ylabel('Cluster power [kW]', fontsize=DefFontSize * 1.2, color=EVSEcolors[1])
-# Combine lines and labels from both axes for the legend
-lines = [line1, line2]
-labels = [l.get_label() for l in lines]
-ax1.legend(lines, labels, loc="best")
-# Date format for the first subplot
-date_format_long = mdates.DateFormatter('%H:%M')
-ax2.xaxis.set_major_locator(mdates.MinuteLocator(interval=2))
-ax2.xaxis.set_major_formatter(date_format_long)
-plt.xticks(rotation=90)
+def process_data():
 
-# Second subplot
-ax5.grid(True)
-line3, = ax5.plot(time_PIOT_short, data_PIOT_short['REG_FREQUENCY'], label='Measured frequency', color=EVSEcolors[0])
-ax5.set_ylim(49.950, 50.075) 
-ax5.set_ylabel('Frequency [Hz]', fontsize=DefFontSize * 1.2, color=EVSEcolors[0])
-ax5.set_xlabel('Time [mm:ss]', fontsize=DefFontSize * 1)
-xticks = ax5.get_xticks()
-xticklabels = ax5.get_xticklabels()
-ax5.set_xticks(xticks)
-ax5.set_xticklabels(xticklabels, rotation=90)
-ax6 = ax5.twinx()
-line4, = ax6.plot(time_PIOT_short, -data_PIOT_short['REG_LINE_ACP_1'] - data_PIOT_short['REG_LINE_ACP_2'] - data_PIOT_short['REG_LINE_ACP_3'], color=EVSEcolors[1], label='Cluster Power')
-ax6.set_ylim(13.5, 17)
-ax6.set_ylabel('Cluster power [kW]', fontsize=DefFontSize * 1.2, color=EVSEcolors[1])
+    
+    # Read the data
+    data_PIOT = pd.read_csv(file_path)
 
-# Combine lines and labels from both axes for the legend
-lines1 = [line3, line4]
-labels = [l.get_label() for l in lines1]
-ax5.legend(lines1, labels, loc="best")
+    # Long time period
+    t_start_long = data_PIOT['timestamp'].iloc[start_long_index]
+    t_end_long = data_PIOT['timestamp'].iloc[end_long_index]
+    data_PIOT_long = data_PIOT[(data_PIOT['timestamp'] >= t_start_long) & (data_PIOT['timestamp'] <= t_end_long)]
+    time_PIOT_long = pd.to_datetime(data_PIOT_long['timestamp'], unit='ms')
 
-# Date format for the second subplot
-date_format_short = mdates.DateFormatter('%M:%S')
-ax6.xaxis.set_major_locator(mdates.SecondLocator(interval=7))
-ax6.xaxis.set_major_formatter(date_format_short)
-plt.xticks(rotation=90)
-plt.tight_layout()
-plt.savefig('./graphs/Combined_Power_long_short.pdf')
+    # Short time period
+    t_start_short = data_PIOT['timestamp'].iloc[start_short_index]
+    t_end_short = data_PIOT['timestamp'].iloc[end_short_index]
+    data_PIOT_short = data_PIOT[(data_PIOT['timestamp'] >= t_start_short) & (data_PIOT['timestamp'] <= t_end_short)]
+    time_PIOT_short = pd.to_datetime(data_PIOT_short['timestamp'], unit='ms')
+
+    # Calculate ACP and frequency
+    ACP = -data_PIOT_long['REG_LINE_ACP_1'] - data_PIOT_long['REG_LINE_ACP_2'] - data_PIOT_long['REG_LINE_ACP_3']
+    freq = data_PIOT_long['REG_FREQUENCY']
+    delay = time_PIOT_long.diff()
+
+    return data_PIOT_long,data_PIOT_short,time_PIOT_long, time_PIOT_short, ACP, freq
 
 
-fig, (ax3, ax4) = plt.subplots(nrows=2, ncols=1, figsize=(15, 7), sharex=True)
+def normalize_freq_2_power():
+    
+    
+def compute_crosscorrelation():
+    cross_correlation = np.correlate(ACP - np.mean(ACP),freq- np.mean(freq),  mode='full')
+    lags = np.arange(-(len(freq) - 1), len(freq))
+    
+    plt.figure(figsize=(10, 6))
+    plt.plot(lags, cross_correlation-60)
+    plt.xlim(0, 16) 
+    
+    plt.title('Cross-correlation between Curve 1 and Curve 2')
+    plt.xlabel('Lags')
+    plt.ylabel('Cross-correlation')
+    plt.grid(True)
+    plt.savefig('./graphs/Cross_corr.pdf')
+    
+    lag_at_max_corr = lags[np.argmax(cross_correlation)]
+    print("The lag is:", lag_at_max_corr, "time units")
+    
+    rmse = np.sqrt(np.mean((np.array(ACP) - np.array(freq))**2))
+    average_error = rmse / len(ACP)
+    
 
-# First subplot for Priority
-ax3.grid(True)
-ax3.plot(time_PIOT_long, data_PIOT_long['Plug2_priority_broadcast'], label='Priority of EV1', color=EVSEcolors[0])
-ax3.plot(time_PIOT_long, data_PIOT_long['Plug3_priority_broadcast'], label='Priority of EV2', color=EVSEcolors[1])
-ax3.set_ylabel('Priority of the EV', fontsize=DefFontSize * 1)
-ax3.legend(loc="best")
+def plot_P_vs_freq():
+    fig, (ax1, ax5) = plt.subplots(nrows=2, ncols=1, figsize=(15, 7), sharex=False)
+    # First subplot
+    ax1.grid(True)
+    line1, = ax1.plot(time_PIOT_long, data_PIOT_long['REG_FREQUENCY'], label='Measured frequency', color=EVSEcolors[0])
+    ax1.set_ylim(49.9, 50.1) 
+    ax1.set_ylabel('Frequency [Hz]', fontsize=DefFontSize * 1.2, color=EVSEcolors[0])
+    ax1.set_xlabel('Time [hh:mm]', fontsize=DefFontSize * 1)
+    xticks = ax1.get_xticks()
+    xticklabels = ax1.get_xticklabels()
+    ax1.set_xticks(xticks)
+    ax1.set_xticklabels(xticklabels, rotation=90)
+    ax2 = ax1.twinx()
+    line2, = ax2.plot(time_PIOT_long, -data_PIOT_long['REG_LINE_ACP_1'] - data_PIOT_long['REG_LINE_ACP_2'] - data_PIOT_long['REG_LINE_ACP_3'], color=EVSEcolors[1], label='Cluster Power')
+    ax2.set_ylim(12.25, 17.5)
+    ax2.set_ylabel('Cluster power [kW]', fontsize=DefFontSize * 1.2, color=EVSEcolors[1])
+    # Combine lines and labels from both axes for the legend
+    lines = [line1, line2]
+    labels = [l.get_label() for l in lines]
+    ax1.legend(lines, labels, loc="best")
+    # Date format for the first subplot
+    date_format_long = mdates.DateFormatter('%H:%M')
+    ax2.xaxis.set_major_locator(mdates.MinuteLocator(interval=2))
+    ax2.xaxis.set_major_formatter(date_format_long)
+    plt.xticks(rotation=90)
+    
+    # Second subplot
+    ax5.grid(True)
+    line3, = ax5.plot(time_PIOT_short, data_PIOT_short['REG_FREQUENCY'], label='Measured frequency', color=EVSEcolors[0])
+    ax5.set_ylim(49.950, 50.075) 
+    ax5.set_ylabel('Frequency [Hz]', fontsize=DefFontSize * 1.2, color=EVSEcolors[0])
+    ax5.set_xlabel('Time [mm:ss]', fontsize=DefFontSize * 1)
+    xticks = ax5.get_xticks()
+    xticklabels = ax5.get_xticklabels()
+    ax5.set_xticks(xticks)
+    ax5.set_xticklabels(xticklabels, rotation=90)
+    ax6 = ax5.twinx()
+    line4, = ax6.plot(time_PIOT_short, -data_PIOT_short['REG_LINE_ACP_1'] - data_PIOT_short['REG_LINE_ACP_2'] - data_PIOT_short['REG_LINE_ACP_3'], color=EVSEcolors[1], label='Cluster Power')
+    ax6.set_ylim(13.5, 17)
+    ax6.set_ylabel('Cluster power [kW]', fontsize=DefFontSize * 1.2, color=EVSEcolors[1])
+    
+    # Combine lines and labels from both axes for the legend
+    lines1 = [line3, line4]
+    labels = [l.get_label() for l in lines1]
+    ax5.legend(lines1, labels, loc="best")
+    
+    # Date format for the second subplot
+    date_format_short = mdates.DateFormatter('%M:%S')
+    ax6.xaxis.set_major_locator(mdates.SecondLocator(interval=7))
+    ax6.xaxis.set_major_formatter(date_format_short)
+    plt.xticks(rotation=90)
+    plt.tight_layout()
+    plt.savefig('./graphs/Combined_Power_long_short.pdf')
 
-# Second subplot for Power
-ax4.grid(True)
-ax4.plot(time_PIOT_long, data_PIOT_long['Plug2_measured_P'], label='Power to EV1', color=EVSEcolors[0])
-ax4.plot(time_PIOT_long, data_PIOT_long['Plug3_measured_P'], label='Power to EV2', color=EVSEcolors[1])
-ax4.set_ylabel('Consumed EV \n Power [kW]', fontsize=DefFontSize * 1)
-ax4.set_xlabel('Time [h:m]', fontsize=DefFontSize * 1)
-ax4.legend(loc="best")
+def plot_individual_values():
+    fig, (ax3, ax4) = plt.subplots(nrows=2, ncols=1, figsize=(15, 7), sharex=True)
+    
+    # First subplot for Priority
+    ax3.grid(True)
+    ax3.plot(time_PIOT_long, data_PIOT_long['Plug2_priority_broadcast'], label='Priority of EV1', color=EVSEcolors[0])
+    ax3.plot(time_PIOT_long, data_PIOT_long['Plug3_priority_broadcast'], label='Priority of EV2', color=EVSEcolors[1])
+    ax3.set_ylabel('Priority of the EV', fontsize=DefFontSize * 1)
+    ax3.legend(loc="best")
+    
+    # Second subplot for Power
+    ax4.grid(True)
+    ax4.plot(time_PIOT_long, data_PIOT_long['Plug2_measured_P'], label='Power to EV1', color=EVSEcolors[0])
+    ax4.plot(time_PIOT_long, data_PIOT_long['Plug3_measured_P'], label='Power to EV2', color=EVSEcolors[1])
+    ax4.set_ylabel('Consumed EV \n Power [kW]', fontsize=DefFontSize * 1)
+    ax4.set_xlabel('Time [h:m]', fontsize=DefFontSize * 1)
+    ax4.legend(loc="best")
+    
+    # Set date format for x-axis
+    date_format = mdates.DateFormatter('%H:%M')
+    ax4.xaxis.set_major_locator(mdates.MinuteLocator(interval=2))
+    ax4.xaxis.set_major_formatter(date_format)
+    plt.xticks(rotation=90)
+    
+    plt.tight_layout()
+    plt.savefig('./graphs/Combined_Priorities_and_Power.pdf')
+    plt.show()
 
-# Set date format for x-axis
-date_format = mdates.DateFormatter('%H:%M')
-ax4.xaxis.set_major_locator(mdates.MinuteLocator(interval=2))
-ax4.xaxis.set_major_formatter(date_format)
-plt.xticks(rotation=90)
 
-plt.tight_layout()
-plt.savefig('./graphs/Combined_Priorities_and_Power.pdf')
-plt.show()
 
+[data_PIOT_long,data_PIOT_short,time_PIOT_long, time_PIOT_short, ACP, freq] = process_data()
+compute_crosscorrelation()
+if include_plot_P_vs_freq:
+    plot_P_vs_freq()
+if include_plot_individual_values:
+    plot_individual_values()
 
 '''fig, ax3 = plt.subplots(figsize=(15, 5))
 plt.grid(True)
